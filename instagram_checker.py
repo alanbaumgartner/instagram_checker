3c748ec71d4133885723b73b42fa8d2c567bbd5f
Alan Baumgartner <AlanBaumgartner@users.noreply.github.com>
2017-03-22 19:22:14 -0500

    Update instagram_checker.py

--- instagram_checker.py
+++ instagram_checker.py
@@ -1,107 +1,99 @@
-import argparse, sys, asyncio, aiohttp, time
+#!/usr/bin/env python3
+import aiohttp, asyncio, argparse
 
-class checker:
-    def __init__(self):
+async def check_usernames(url, igname, igpass, username, sem, session, loop=None):
+    async with sem:
+        try:
+            url = url.format(username)
+            async with session.get(url) as resp:
+                text = await resp.text()
+                text = text[text.find('<title>') + 7 :text.find('</title>')]
+                if "Page Not Found" in text:
+                    save_username(username, outputfile)
+        except Exception as e:
+            print(e)
 
-        #Gets username, password, textfile to check usernames, and output file for available usernames.
-        parser = argparse.ArgumentParser()
-        parser.add_argument("-u", dest='username', help="Instagram username",
-                            action="store")
-        parser.add_argument("-p", dest='password', help="Instagram password",
-                            action="store")
-        parser.add_argument("-i", dest='inputf', help="Textfile with usernames",
-                            action="store")
-        parser.add_argument("-o", dest='outputf', help="Output textfile",
-                            action="store")
-        args = parser.parse_args()
+async def start_check(url, igname, igpass, usernames, conns=20, loop=None):
+    sem = asyncio.BoundedSemaphore(conns)
+    async with aiohttp.ClientSession(loop=loop) as session:
+        await login(url, igname, igpass, session)
+        tasks = [check_usernames(url, igname, igpass, username, sem, session, loop=loop) for username in usernames]
+        await asyncio.gather(*tasks)
 
-        #Save variables from argparse
-        self.username = args.username
-        self.password = args.password
-        self.inputf = args.inputf
-        self.outputf = args.outputf
+def get_usernames(inputfile):
+    #Gets username from file
+    with open(inputfile, "r") as f:
+        usernames = f.read().split("\n")
+        return usernames
 
-        self.loginurl = 'https://www.instagram.com/accounts/login/ajax/'
-        self.url = 'https://www.instagram.com/{}'
+def save_username(username, outputfile):
+    #checks username and saves available usernames to new file
+    with open(outputfile, "a") as a:
+        a.write(username+'\n')
 
-    async def login(self, session, username, password):
-        #Logs into instagram
-        url = self.url.format('')
+async def login(url, username, password, session):
+    #Logs into instagram
+    url = url.format('')
 
-        async with session.get(url) as response:
-            csrftoken = await response.text()
+    async with session.get(url) as response:
+        csrftoken = await response.text()
 
-        csrftoken = csrftoken.split('csrf_token": "')[1].split('"')[0]
+    csrftoken = csrftoken.split('csrf_token": "')[1].split('"')[0]
 
-        async with session.post(
-                self.loginurl,
-                    headers={
-                        'x-csrftoken': csrftoken,
-                        'x-instagram-ajax':'1',
-                        'x-requested-with': 'XMLHttpRequest',
-                        'Origin': url,
-                        'Referer': url,
-                    },
-                    data={
-                        'username':username,
-                        'password':password,
-                    }
-                ) as response:
+    async with session.post(
+            loginurl,
+                headers={
+                    'x-csrftoken': csrftoken,
+                    'x-instagram-ajax':'1',
+                    'x-requested-with': 'XMLHttpRequest',
+                    'Origin': url,
+                    'Referer': url,
+                },
+                data={
+                    'username':username,
+                    'password':password,
+                }
+            ) as response:
 
-                text = await response.json()
-                if 'authenticated' in text:
-                    print('Logged In.')
-                else:
-                    print(text)
-                    sys.exit("Login Failed, closing program.")
-        
-    def get_usernames(self, input):
-        #Gets username from file
-        with open(input, "r") as f:
-            usernames = f.read().split("\n")
-            return usernames
+            text = await response.json()
+            if 'authenticated' in text:
+                #print('Logged In.')
+                pass
+            else:
+                print(text)
 
-    def save_username(self, username, output):
-        #checks username and saves available usernames to new file
-        with open(output, "a") as a:
-            a.write(user+'\n')
+def main(url, igname, igpass):
+    loop = asyncio.get_event_loop()
+    usernames = get_usernames(inputfile)
+    try:
+        loop.run_until_complete(start_check(url, igname, igpass, usernames))
+    finally:
+        loop.close()
 
-    async def check_username(self, session, username):
-        #checks username and saves available usernames to new file
-        async with session.get(self.url.format(username)) as response:
-            text = await response.text()
-            text = text[text.find('<title>') + 7 :text.find('</title>')]
-            if "Page Not Found" in text:
-                return self.save_username(username, self.outputf)
-
-    async def setup_usernames(self, session, usernames):
-        #Sets up tasks to check usernames
-        count = 0
-        total = len(usernames)
-        for username in usernames:
-            count += 1
-            await asyncio.ensure_future(self.check_username(session, username))
-            if count % 25 == 0:
-                print('Tested', count, 'of', total)
-
-    async def main(self, session, loop):
-        await self.login(session, self.username, self.password)
-        usernames = self.get_usernames(self.inputf)
-        await self.setup_usernames(session, usernames)
 
 if __name__ == "__main__":
-    check = checker()
 
-    #Clears output file for new usernames
-    with open(check.outputf, "w") as a:
-        print('Output file cleared.')
+    #Gets values from command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", dest='username', help="Instagram username",
+                        action="store")
+    parser.add_argument("-p", dest='password', help="Instagram password",
+                        action="store")
+    parser.add_argument("-i", dest='inputfile', help="Textfile with usernames",
+                        action="store")
+    parser.add_argument("-o", dest='outputfile', help="Output textfile",
+                        action="store")
+    args = parser.parse_args()
 
-    loop = asyncio.get_event_loop()
+    #Save variables from command line
+    username = args.username
+    password = args.password
+    inputfile = args.inputfile
+    outputfile = args.outputfile
 
-    session = aiohttp.ClientSession(loop=loop)
+    #Default URL variables
+    loginurl = 'https://www.instagram.com/accounts/login/ajax/'
+    url = 'https://www.instagram.com/{}'
 
-    tasks = [
-        asyncio.ensure_future(check.main(session, loop))
-    ]
-
-    loop.run_until_complete(asyncio.wait(tasks))
+    #Runs program
+    main(url, username, password)
