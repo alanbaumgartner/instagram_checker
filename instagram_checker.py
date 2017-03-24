5a7f42428ccabc840d98a79a0981d801e161dda1
Alan Baumgartner <AlanBaumgartner@users.noreply.github.com>
2017-03-23 20:33:03 -0500

    Update instagram_checker.py

--- instagram_checker.py
+++ instagram_checker.py
@@ -1,4 +1,13 @@
-import aiohttp, asyncio, argparse
+import aiohttp, asyncio, argparse, re
+
+def get_usernames(inputfile):
+    with open(inputfile, "r") as f:
+        usernames = f.read().split("\n")
+        return usernames
+
+def save_username(username, outputfile):
+    with open(outputfile, "a") as a:
+        a.write(username+'\n')
 
 async def check_usernames(igname, igpass, username, sem, session, loop=None):
     async with sem:
@@ -12,30 +21,21 @@ async def check_usernames(igname, igpass, username, sem, session, loop=None):
         except Exception:
             print(Exception)
 
-async def start_check(igname, igpass, usernames, conns=50, loop=None):
+async def start_check(igname, igpass, conns=50, loop=None):
     sem = asyncio.BoundedSemaphore(conns)
     async with aiohttp.ClientSession(loop=loop) as session:
         await login(igname, igpass, session)
+        usernames = get_usernames(inputfile)
         tasks = [check_usernames(igname, igpass, username, sem, session, loop=loop) for username in usernames]
         await asyncio.gather(*tasks)
 
-def get_usernames(inputfile):
-    with open(inputfile, "r") as f:
-        usernames = f.read().split("\n")
-        return usernames
-
-def save_username(username, outputfile):
-    with open(outputfile, "a") as a:
-        a.write(username+'\n')
-
 async def login(username, password, session):
     loginurl = 'https://www.instagram.com/accounts/login/ajax/'
-    url = 'https://www.instagram.com/{}'
-    url = url.format('')
+    url = 'https://www.instagram.com/'
 
     async with session.get(url) as response:
         csrftoken = await response.text()
-
+        
     csrftoken = csrftoken.split('csrf_token": "')[1].split('"')[0]
 
     async with session.post(
@@ -61,22 +61,19 @@ async def login(username, password, session):
 
 def main(igname, igpass):
     loop = asyncio.get_event_loop()
-    usernames = get_usernames(inputfile)
     try:
-        loop.run_until_complete(start_check(igname, igpass, usernames))
+        loop.run_until_complete(start_check(igname, igpass))
     finally:
         loop.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", dest='username', help="Instagram username",
-                        action="store")
-    parser.add_argument("-p", dest='password', help="Instagram password",
-                        action="store")
-    parser.add_argument("-i", dest='inputfile', help="Textfile with usernames",
-                        action="store")
-    parser.add_argument("-o", dest='outputfile', help="Output textfile",
-                        action="store")
+
+    parser.add_argument("-u", dest='username', action="store")
+    parser.add_argument("-p", dest='password', action="store")
+    parser.add_argument("-i", dest='inputfile', action="store")
+    parser.add_argument("-o", dest='outputfile', action="store")
+
     args = parser.parse_args()
 
     igname = args.username
@@ -86,5 +83,5 @@ if __name__ == "__main__":
 
     with open(outputfile, "w") as a:
         print('Output file cleared.')
-
+    
     main(igname, igpass)
